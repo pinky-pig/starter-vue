da793a31077b6927d1ce3838919b7518926255d4
pinky-pig <2545589993@qq.com>
2024-05-14 16:19:44 +0800

    fix: fixed typo

--- packages/materials/src/lib/some-buttons/SketchButton.vue
+++ packages/materials/src/lib/some-buttons/SketchButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <button class="px-4 py-2 rounded-md border border-black bg-white text-neutarl-700 text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200 flex justify-center items-center">
+  <button class="px-4 py-2 rounded-md border border-black text-black bg-white text-neutarl-700 text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200 flex justify-center items-center">
     <slot />
   </button>
 </template>
