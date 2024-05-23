0b8853d82f6d3a2c63b73c973b0f5531fb40650c
pinky-pig <2545589993@qq.com>
2024-05-23 14:43:38 +0800

    feat: 添加 Overlay Left button

--- packages/materials/src/index.ts
+++ packages/materials/src/index.ts
@@ -2,7 +2,7 @@ import FlipCard from './lib/flip-card'
 import WireCard from './lib/wire-card'
 import Meteors from './lib/meteors'
 import WavyBackground from './lib/wavy-background'
-import { BevelButton, GenerateButton, SketchBlockButton, SketchButton } from './lib/some-buttons'
+import { BevelButton, GenerateButton, OverlayLeft, SketchBlockButton, SketchButton } from './lib/some-buttons'
 
 export {
   FlipCard,
@@ -13,6 +13,7 @@ export {
   Meteors,
   WireCard,
   WavyBackground,
+  OverlayLeft,
 }
 
 export * from './types'
--- packages/materials/src/lib/some-buttons/index.ts
+++ packages/materials/src/lib/some-buttons/index.ts
@@ -2,5 +2,12 @@ import GenerateButton from './GenerateButton.vue'
 import SketchButton from './SketchButton.vue'
 import SketchBlockButton from './SketchBlockButton.vue'
 import BevelButton from './BevelButton.vue'
+import OverlayLeft from './OverlayLeft.vue'
 
-export { GenerateButton, SketchButton, SketchBlockButton, BevelButton }
+export {
+  GenerateButton,
+  SketchButton,
+  SketchBlockButton,
+  BevelButton,
+  OverlayLeft,
+}
